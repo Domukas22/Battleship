7d2ce2ebeead7ec6f7c809309b802d15229ffdd6
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-10 18:52:44 +0200

    Add Eslint and Prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import returnText from "./logic/function";
-import './styles/reset.css'
-import './styles/main.css'
+import "./styles/reset.css";
+import "./styles/main.css";
 
-
-function print() {
-    const text = document.createElement('h2')
-    text.innerHTML = returnText()
-    document.body.appendChild(text)
+function print(textSTRING) {
+  const text = document.createElement("h2");
+  text.innerHTML = textSTRING;
+  document.body.appendChild(text);
 }
 
-print();
+print(returnText());
+print("hello there");
