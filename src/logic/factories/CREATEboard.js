743c57174dd1f67c3a59aded033f625d97af6bbb
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-28 14:18:57 +0200

    Add board functions and tests

--- src/logic/factories/CREATEboard.js
+++ src/logic/factories/CREATEboard.js
@@ -6,35 +6,34 @@ import CREATEship from "./CREATEship";
 //
 export default function CREATEboard() {
   const shipOBJS = [];
-  const hitLIST = [];
+  const hitLIST = new Set();
 
   const PLACEship = (startCORDS, endCORDS) => {
     shipOBJS.push(CREATEship(startCORDS, endCORDS));
   };
+  const ISgameOver = function AREallShipsSunk() {
+    return shipOBJS.every((ship) => ship.isSunk() === true);
+  };
 
   const RECEIVEattack = (x, y) => {
+    if (!_AREcordsValid(x, y)) return "X and Y must be between 0 and 9";
     if (_AREcordsOnHitList(x, y)) return "Already hit";
+
+    const newHitCORDS = [x, y];
+    hitLIST.add(newHitCORDS.toString());
 
     if (_AREcordsOnShip(x, y)) {
       const targetSHIP = _GETshipWithCords(x, y);
       targetSHIP.hit(x, y);
+      if (targetSHIP.isSunk()) {
+        _HANLDEsunkenShip(targetSHIP);
+      }
     }
-    hitLIST.push([x, y]);
+  };
 
-    // if shipOBJS has cords
-    // ship.hit()
-    // if ship.isSunk() === true
-    // disable surrounding water cords
-    // if all ships are sunk
-    // end game
-    // if missedATTACKS has cords
-    // return false
-    // missedATTACKS.push(x, y)
-  };
-  const ISgameOver = function AREallShipsSunk() {
-    return shipOBJS.every((ship) => ship.isSunk() === true);
-  };
   const GETships = () => [...shipOBJS];
+  const GEThitList = () => [...hitLIST];
+
   function _AREcordsOnShip(x, y) {
     return shipOBJS.some((ship) => ship.HAScords(x, y) === true);
   }
@@ -42,12 +41,35 @@ export default function CREATEboard() {
     return shipOBJS.find((ship) => ship.HAScords(x, y) === true);
   }
   function _AREcordsOnHitList(x, y) {
-    return hitLIST.some((hit) => hit[0] === x && hit[1] === y);
+    return hitLIST.has([x, y].toString());
+  }
+  function _AREcordsValid(x, y) {
+    return x >= 0 && x <= 9 && y >= 0 && y <= 9;
+  }
+  function _HANLDEsunkenShip(sunkenShipOBJ) {
+    const sunkenShipCORDS = sunkenShipOBJ.GETcords().map((cord) => cord.cell);
+    sunkenShipCORDS.forEach((cords) => {
+      const surroundingCORDS = _GETsurroundingCords(cords[0], cords[1]);
+      surroundingCORDS.forEach((surrCORDS) => hitLIST.add(surrCORDS.toString()));
+    });
+  }
+  function _GETsurroundingCords(x, y) {
+    return [
+      [x + 1, y],
+      [x - 1, y],
+      [x, y + 1],
+      [x, y - 1],
+      [x - 1, y + 1],
+      [x - 1, y - 1],
+      [x + 1, y + 1],
+      [x + 1, y - 1],
+    ];
   }
   return {
     PLACEship,
     GETships,
     ISgameOver,
     RECEIVEattack,
+    GEThitList,
   };
 }
--- src/logic/factories/__tests__/CREATEboard.test.js
+++ src/logic/factories/__tests__/CREATEboard.test.js
@@ -35,16 +35,44 @@ describe("Board attacks", () => {
   beforeEach(() => {
     board = CREATEboard();
   });
+  test("Attacks with false cords", () => {
+    // place ship and attack first cell
+    expect(board.RECEIVEattack(-1, 1)).toBe("X and Y must be between 0 and 9");
+    expect(board.RECEIVEattack(5, 10)).toBe("X and Y must be between 0 and 9");
+  });
   test("Attacks an already hit spot", () => {
     // place ship and attack first cell
     board.PLACEship([1, 1], [1, 2]);
     board.RECEIVEattack(1, 1);
     expect(board.RECEIVEattack(1, 1)).toBe("Already hit");
   });
-  test("Attacks a ship", () => {
+  test("Attacks ship", () => {
     // place ship, attack first cell, check ship cell status
     board.PLACEship([1, 1], [1, 2]);
     board.RECEIVEattack(1, 1);
     expect(board.GETships()[0].GETcords()[0].hit).toBe(true);
+  });
+  test("Sinks ship and hits surrouding cordinates", () => {
+    // place ship, attack first cell, check ship cell status
+    board.PLACEship([1, 1], [1, 2]);
+    board.RECEIVEattack(1, 1);
+    board.RECEIVEattack(1, 2);
+    expect(board.GETships()[0].isSunk()).toBe(true);
+    const hitLIST = board.GEThitList();
+    expect(hitLIST).toContain("0,0");
+    expect(hitLIST).toContain("0,3");
+    expect(hitLIST).toContain("2,3");
+    expect(hitLIST).toContain("2,0");
+  });
+  test("Attacks and ends game", () => {
+    // place ship, attack first cell, check ship cell status
+    board.PLACEship([1, 1], [1, 2]);
+    board.PLACEship([6, 2], [6, 4]);
+    board.RECEIVEattack(1, 1);
+    board.RECEIVEattack(1, 2);
+    board.RECEIVEattack(6, 2);
+    board.RECEIVEattack(6, 3);
+    board.RECEIVEattack(6, 4);
+    expect(board.ISgameOver()).toBe(true);
   });
 });
